c9e0518061449a3833e93a8fdf930fbcc8aa719e
Divyanshu <sdivyanshu105@gmail.com>
2022-06-23 03:41:04 +0530

    new 2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function greet(){
-    alert('hi there');
+    alert('Hi there thank you for choosing our service!');
 }
 
-const b=document.querySelector('button');
-
+function bye(){
+    alert('Oh shuks, but Hope to see you onboard next time!');
+}
+const b=document.getElementById('one');
+const b1=document.getElementById('two');
 b.addEventListener('click',greet);
+b1.addEventListener('click',bye);
 
